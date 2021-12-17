a69d9d8d8f4a145ea6d93346a4d5fc16bcca5ced
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2021-12-18 02:05:30 +0900

    docs: update sample

--- sample1.py
+++ sample1.py
@@ -23,7 +23,7 @@ print("v2.name=%s" % repr(v2.name))
 print("v2.max_length=%s" % repr(v2.max_length))
 print("v2.color=%s" % repr(v2.color))
 
-# Initialize from docopt's return value, with casting str to int
+# Initialize from docopt's return value, with casting str to int.
 v3: MyArgs = MyArgs(_cast_str_values=True, **{'<count>': '1', '--name': "Joe", '--max-length': '100', '--color': 'RED'})
 
 print("dir(v3)=%s" % repr(dir(v3)))
@@ -32,7 +32,17 @@ print("v3.name=%s" % repr(v3.name))
 print("v3.max_length=%s" % repr(v3.max_length))
 print("v3.color=%s" % repr(v3.color))
 
+# Options and positional arguments are fine as long as their names do not conflict.
+v4: MyArgs = MyArgs(_cast_str_values=True, **{'--count': '1', '<name>': "Joe", '--max_length': '100', '-color': 'RED'})
+
+print("dir(v4)=%s" % repr(dir(v4)))
+print("v4.count=%s" % repr(v4.count))
+print("v4.name=%s" % repr(v4.name))
+print("v4.max_length=%s" % repr(v4.max_length))
+print("v4.color=%s" % repr(v4.color))
+
 # Error checking
 # MyArgs(_cast_str_values=True, **{'--nam': "Joe"})  # raises KeyError: "attribute `'nam'` not found in class `<class '__main__.MyArgs'>`"
 # MyArgs(_cast_str_values=True, **{'<count>': "vii"})  # raises ValueError: invalid literal for int() with base 10: 'vii'
+# MyArgs(_cast_str_values=True, **{'--color': "GLAY"})  # raises ValueError: Invalid Enum name: 'GLAY'
 # MyArgs(_cast_str_values=True, **{'---x': "vii"})  # raises NameError: Invalid name for option or positional argument: '---x'
