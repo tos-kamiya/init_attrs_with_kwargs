647ba2a0f69bed5e57db979ef8c3f39d60df2382
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2022-03-27 13:39:37 +0900

    fix: add docstrings

--- init_attrs_with_kwargs/__init__.py
+++ init_attrs_with_kwargs/__init__.py
@@ -1,4 +1,4 @@
-from typing import get_type_hints, get_origin, get_args, Any, Dict, TypeVar, Union
+from typing import get_type_hints, get_origin, get_args, Any, Optional, TypeVar, Union
 from enum import Enum
 
 
@@ -43,7 +43,21 @@ class InitAttrsWKwArgs:
         InitAttrsWKwArgs.set_attrs(self, cast_str_values=_cast_str_values, **kwargs)
 
     @staticmethod
-    def set_attrs(target: T, cast_str_values: bool = False, **kwargs: Any) -> T:
+    def set_attrs(target: T, cast_str_values: Optional[bool] = False, **kwargs: Any) -> T:
+        """Assign target object's attributes with keyword arguments.
+
+        Args:
+            target: target object whose attributes are changed.
+            cast_str_values: Optional. if True, when an assingned value is str, convert it into either bool, 
+              int, float, or list of these types, depending on types of the target attribute.
+            kwargs: values to be assigned to target attributes.
+
+        Returns:
+            target object.
+        
+        Raises:
+            KeyError: In case target object does not have such attribute. In case wrong enum name.
+        """
         attr_to_type = get_type_hints(target.__class__)
         for name in kwargs:
             attr = InitAttrsWKwArgs._convert_option_name_to_attr_name(name)
@@ -76,4 +90,16 @@ class InitAttrsWKwArgs:
 set_attrs = InitAttrsWKwArgs.set_attrs
 
 def cast_set_attrs(target: T, **kwargs: Any) -> T:
+    """Assign target object's attributes with keyword arguments, with converting str values.
+
+    Args:
+        target: target object whose attributes are changed.
+        kwargs: values to be assigned to target attributes.
+
+    Returns:
+        target object.
+    
+    Raises:
+        KeyError: In case target object does not have such attribute. In case wrong enum name.
+    """
     return InitAttrsWKwArgs.set_attrs(target, cast_str_values = True, **kwargs)
